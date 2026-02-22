7de1abd1eadfdeb423ef37f151d9899d10590092
bharathiraja-15 <bharathimtech11@gmail.com>
2026-02-22 23:32:01 +0530

    Update DatabaseAccess.java

--- src/main/java/ca/sheridancollege/database/DatabaseAccess.java
+++ src/main/java/ca/sheridancollege/database/DatabaseAccess.java
@@ -2,6 +2,8 @@ package ca.sheridancollege.database;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -10,25 +12,68 @@ import org.springframework.stereotype.Repository;
 import ca.sheridancollege.beans.Mission;
 
 /**
- * This is the database class. It includes the data and methods to manipulate
- * the data.
- * 
- * @author Mihye 
+ * Database access class
  */
 @Repository
 public class DatabaseAccess {
-	
+
 	private NamedParameterJdbcTemplate jdbc;
 
 	public DatabaseAccess(NamedParameterJdbcTemplate jdbc) {
 		this.jdbc = jdbc;
 	}
 
-	public int addMission(Mission mission) {
-		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+	/* =====================================================
+	   ✅ INITIAL DATA FOR JENKINS TEST ENVIRONMENT
+	   ===================================================== */
+	@PostConstruct
+	public void init() {
 
-		String query = "INSERT INTO missions (agent, title, gadget1, gadget2) "
+		MapSqlParameterSource params =
+				new MapSqlParameterSource();
+
+		String countQuery =
+				"SELECT COUNT(*) FROM missions";
+
+		Integer count =
+				jdbc.queryForObject(
+						countQuery,
+						params,
+						Integer.class);
+
+		// insert default data only if table empty
+		if (count == 0) {
+
+			String insert =
+				"INSERT INTO missions (agent, title, gadget1, gadget2) "
 				+ "VALUES (:agent, :title, :gadget1, :gadget2)";
+
+			jdbc.update(insert,
+					new MapSqlParameterSource()
+					.addValue("agent", "Johnny English")
+					.addValue("title", "Rescue the Queen")
+					.addValue("gadget1", "Exploding Cigar")
+					.addValue("gadget2", "Rolls Royce"));
+
+			jdbc.update(insert,
+					new MapSqlParameterSource()
+					.addValue("agent", "Natasha Romanova")
+					.addValue("title", "Secret Mission")
+					.addValue("gadget1", "Widow Bite")
+					.addValue("gadget2", "Tracker"));
+		}
+	}
+
+	/* ===================================================== */
+
+	public int addMission(Mission mission) {
+
+		MapSqlParameterSource namedParameters =
+				new MapSqlParameterSource();
+
+		String query =
+			"INSERT INTO missions (agent, title, gadget1, gadget2) "
+			+ "VALUES (:agent, :title, :gadget1, :gadget2)";
 
 		namedParameters
 			.addValue("agent", mission.getAgent())
@@ -36,38 +81,41 @@ public class DatabaseAccess {
 			.addValue("gadget1", mission.getGadget1())
 			.addValue("gadget2", mission.getGadget2());
 
-		int returnValue = jdbc.update(query, namedParameters);
-		return returnValue;
+		return jdbc.update(query, namedParameters);
 	}
 
 	public Mission getMission(Long id) {
-		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
 
-		String query = "SELECT * FROM missions WHERE id = :id";
+		MapSqlParameterSource namedParameters =
+				new MapSqlParameterSource();
+
+		String query =
+				"SELECT * FROM missions WHERE id = :id";
 
 		namedParameters.addValue("id", id);
 
-		BeanPropertyRowMapper<Mission> missionMapper = 
-				new BeanPropertyRowMapper<Mission>(Mission.class);
+		BeanPropertyRowMapper<Mission> mapper =
+				new BeanPropertyRowMapper<>(Mission.class);
 
-		// now update with the new data and return row
-		List<Mission> missions = 
-				jdbc.query(query, namedParameters, missionMapper);
+		List<Mission> missions =
+				jdbc.query(query, namedParameters, mapper);
 
-		// if the list is empty, return null. Otherwise, return the first one
 		if (missions.isEmpty()) {
-			return null; // error condition
-		} else {
-			return missions.get(0);
+			return null;
 		}
+
+		return missions.get(0);
 	}
 
 	public int updateMission(Mission mission) {
-		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
 
-		String query = 
-				"UPDATE missions SET agent = :agent, title = :title, gadget1 = :gadget1"
-				+ ", gadget2 = :gadget2 WHERE id = :id";
+		MapSqlParameterSource namedParameters =
+				new MapSqlParameterSource();
+
+		String query =
+			"UPDATE missions SET agent=:agent, title=:title, "
+			+ "gadget1=:gadget1, gadget2=:gadget2 "
+			+ "WHERE id=:id";
 
 		namedParameters
 			.addValue("agent", mission.getAgent())
@@ -76,35 +124,35 @@ public class DatabaseAccess {
 			.addValue("gadget2", mission.getGadget2())
 			.addValue("id", mission.getId());
 
-		int returnValue = jdbc.update(query, namedParameters);
-		return returnValue;
+		return jdbc.update(query, namedParameters);
 	}
 
 	public int deleteMission(Long id) {
-		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
 
-		String query = "DELETE FROM missions WHERE id= :id";
+		MapSqlParameterSource namedParameters =
+				new MapSqlParameterSource();
 
-		// add parameters to the map
+		String query =
+				"DELETE FROM missions WHERE id=:id";
+
 		namedParameters.addValue("id", id);
 
-		// now update with the new data and return number of rows affected
-		int returnValue = jdbc.update(query, namedParameters);
-
-		return returnValue;
+		return jdbc.update(query, namedParameters);
 	}
 
 	public List<Mission> getMissions(String agent) {
-		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
-		String query = "SELECT * FROM missions WHERE agent = :agent";
+
+		MapSqlParameterSource namedParameters =
+				new MapSqlParameterSource();
+
+		String query =
+				"SELECT * FROM missions WHERE agent=:agent";
 
 		namedParameters.addValue("agent", agent);
 
-		BeanPropertyRowMapper<Mission> missionMapper = 
-				new BeanPropertyRowMapper<Mission>(Mission.class);
+		BeanPropertyRowMapper<Mission> mapper =
+				new BeanPropertyRowMapper<>(Mission.class);
 
-		List<Mission> missions = 
-				jdbc.query(query, namedParameters, missionMapper);
-		return missions;
+		return jdbc.query(query, namedParameters, mapper);
 	}
 }
